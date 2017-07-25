5cd94c4e4e530d772027118c5e94a0c890621280
wafaklabi <wafa.klabi1@gmail.com>
2017-07-25 13:33:16 +0100

    a

--- product.php
+++ product.php
@@ -88,7 +88,7 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 			<div class="header-bottom">
 			<div class="container">
 				<div class="logo">
-					<h1><a href="index.html"><img src="images/logo-01.png" alt=""></a></h1>
+					<h1><a href="product.php"><img src="images/logo-01.png" alt=""></a></h1>
 				</div>
 
 
@@ -96,8 +96,8 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
 
 			<div class="top-nav">
 				<ul class="memenu skyblue">
-					<li class="active"><a href="index.html" >BookShelf</a></li>
-					<li class="grid"><a href="contact.html">Add a book</a></li>
+					<li class="active"><a href="product.php" >BookShelf</a></li>
+					<li class="grid"><a href="contact.php">Add a book</a></li>
 				</ul>
 				<div class="clearfix"> </div>
 			</div>
